86e4eb17ced7cbec1f3facfc77bc835f851899f1
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-09-01 14:22:43 +0530

    add chrome webdrivers

---
+++ test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from time import sleep
+
+class InstaBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://instagram.com")
+
+InstaBot()        
